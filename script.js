6d67e277e49b8f1e8cac202ab41d23ab6447b2a1
Georgepaulthottam <78588483+Georgepaulthottam@users.noreply.github.com>
2022-07-21 23:25:35 +0530

    committed after editing

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
-}
-        
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    s=(p*r*t)/100;
+    document.getElementById("r").innerHTML ="at an interest rate of "+r+"%";
+    document.getElementById("p").innerHTML = "If you deposit "+p;
+    document.getElementById("s").innerHTML ="You will receive an amount of "+ s;
+    document.getElementById("y").innerHTML = "By The year 2025" ;
+   
+    }
